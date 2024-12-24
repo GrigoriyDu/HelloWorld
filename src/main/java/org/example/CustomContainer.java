b0527b609cb07aef73d2f61104bc0758188306b1
Grigorii Dukach <dukach055@yandex.ru>
2024-12-24 17:52:38 +0300

    Добавление метода insert и метода expandCapacity

--- src/main/java/org/example/CustomContainer.java
+++ src/main/java/org/example/CustomContainer.java
@@ -1,13 +1,43 @@
 package org.example;
 
+/**
+ * Класс CustomContainer представляет собой обобщенный контейнер для хранения элементов.
+ * Контейнер автоматически увеличивает свою емкость при добавлении новых элементов, если текущая емкость исчерпана.
+ * Контейнер позволяет позволять добавлять, извлекать, удалять элементы
+ *
+ * @param <T> Тип элементов, хранящихся в контейнере.
+ */
 public class CustomContainer<T> {
     private Object[] elements;
     private int currentSize;
     private int maxCapacity;
-
+    /**
+     * Конструктор по умолчанию, инициализирующий контейнер с начальной емкостью 10 элементов.
+     */
     public CustomContainer(){
         maxCapacity = 10;
         elements = new Object[maxCapacity];
         currentSize = 0;
+    }
+    /**
+     * Добавляет элемент в контейнер. Если текущая емкость исчерпана, емкость контейнера увеличивается.
+     *
+     * @param item Элемент, который нужно добавить в контейнер.
+     */
+    public void insert(T item) {
+        if (currentSize == maxCapacity) {
+            expandCapacity();
+        }
+        elements[currentSize] = item;
+        currentSize++;
+    }
+    /**
+     * Увеличивает емкость контейнера в два раза.
+     */
+    private void expandCapacity() {
+        maxCapacity *= 2;
+        Object[] newElements = new Object[maxCapacity];
+        System.arraycopy(elements, 0, newElements, 0, currentSize);
+        elements = newElements;
     }
 }

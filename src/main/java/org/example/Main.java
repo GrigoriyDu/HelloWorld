3d4215de274dd3336b1f1ecd41e6419964228f0e
Grigorii Dukach <dukach055@yandex.ru>
2024-12-24 19:07:14 +0300

    Добавление метода displayElements

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,10 +1,30 @@
 package org.example;
 
+/**
+ * Класс MainClass демонстрирует использование класса CustomContainer.
+ */
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World!!!");
+        // Создание экземпляра контейнера
         CustomContainer<Integer> customContainer = new CustomContainer<>();
-        customContainer.insert(10);
-        System.out.println(customContainer.retrieve(0));
+        // Добавление элементов в контейнер
+        customContainer.insert(111);
+        customContainer.insert(666);
+        customContainer.insert(1234);
+        // Отображение текущего размера контейнера
+        System.out.println("Размер контейнера: " + customContainer.getSize());
+        // Отображение всех элементов контейнера
+        customContainer.displayElements();
+        // Получение элемента по индексу
+        System.out.println("Элемент по индексу 1: " + customContainer.retrieve(1));
+        // Удаление элемента по индексу
+        System.out.println("--------------------------");
+        customContainer.delete(1);
+        // Отображение всех элементов контейнера после удаления
+        customContainer.displayElements();
+        // Отображение текущего размера контейнера после удаления
+        System.out.println("Размер контейнера после удаления: " + customContainer.getSize());
+        // Получение элемента по индексу после удаления
+        System.out.println("Элемент по индексу 1 после удаления: " + customContainer.retrieve(1));
     }
 }
---
+++ src/test/java/org/example/CustomContainerTest.java
@@ -0,0 +1,33 @@
+package org.example;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CustomContainerTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void insert() {
+    }
+
+    @Test
+    void retrieve() {
+    }
+
+    @Test
+    void delete() {
+    }
+
+    @Test
+    void getSize() {
+    }
+
+    @Test
+    void displayElements() {
+    }
+}

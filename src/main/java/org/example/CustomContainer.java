09314bac489e8c38bcc27d27af03f3063da818e1
Grigorii Dukach <dukach055@yandex.ru>
2024-12-24 17:40:27 +0300

    Создание базового класса CustomContainer

--- src/main/java/org/example/CustomContainer.java
+++ src/main/java/org/example/CustomContainer.java
@@ -1,4 +1,13 @@
 package org.example;
 
-public class CustomContainer {
+public class CustomContainer<T> {
+    private Object[] elements;
+    private int currentSize;
+    private int maxCapacity;
+
+    public CustomContainer(){
+        maxCapacity = 10;
+        elements = new Object[maxCapacity];
+        currentSize = 0;
+    }
 }
